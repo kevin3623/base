bde0ab47e4489076ba02ae6ce49eb9db8b983262
wufei3623 <1737509789@qq.com>
2020-04-15 11:14:46 +0800

    wufie

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 /**
  * 格式化时间
  * @param {String} formater 日期格式
- * @param {字符串} t 时间戳
+ * @param {字符串} t 创建时间
  */
 function dateFormater(formater, t) {
   let date = t ? new Date(t) : new Date(),
@@ -19,3 +19,20 @@ function dateFormater(formater, t) {
       .replace(/mm/g,(m<10?'0':'') + m)
       .replace(/ss/g,(s<10?'0':'') + s)
 }
+/**
+ * 节流函数 限制函数在指定时间段只能被调用一次
+ * 用法 比如防止用户连续执行一个耗时操作 对操作按钮点击函数进行节流处理
+ * @param {Function} func [需要节流的函数]
+ * @param {Number} wait [节流控制时间]
+ */
+function throttle(func, wait) {
+    let timer = null;
+    return function(...args) {
+      if (!timer) {
+        func(...args);
+        timer = setTimeout(() => {
+          timer = null;
+        }, wait);
+      }
+    }
+  }
